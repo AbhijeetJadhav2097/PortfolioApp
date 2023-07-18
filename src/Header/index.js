8033be622bef448d9caa6ad7d025de839d3c818c
Abhijeet Jadhav <abhijeetjadhav2097@gmail.com>
2023-07-18 21:12:40 +0530

    basic portfolio completed with header

--- src/Header/index.js
+++ src/Header/index.js
@@ -3,9 +3,9 @@ import './index.css'
 const Header = () => {
     return(
         <div className="Header">
-        <h1>Abhijeet Jadhav</h1>
-        <h3>Email: abhijeetjadhav2097@gmail.com</h3>
-        <h3>Phone Number: +91 7620399374</h3>
+        <p className="candidate-name">Abhijeet Jadhav</p>
+        <p className="candidate-email">Email: abhijeetjadhav2097@gmail.com</p>
+        <p className="candidate-phone">Phone Number: +91 7620399374</p>
        </div>
     )
 }
