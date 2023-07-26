1c0451da9b67792c04c62f25d5e101436bdaa9c6
Abhijeet Jadhav <abhijeetjadhav2097@gmail.com>
2023-07-26 14:45:58 +0530

    completed portfolio project

--- src/LeftSection/index.js
+++ src/LeftSection/index.js
@@ -1,20 +1,25 @@
 import './index.css'
-
+import Education from './Education/index'
 const LeftSection = () => {
     return (
         <div className="LeftSection">
             <div className="Education">
                 <p className="section-title">Education</p>
-                <p className="section-sub-title">PES College of Engineering <span className="year">(2016-2020)</span> </p>
-                <p className="section-sub-title">Diploma in Mechatronics <span className="year">(2013-2016)</span></p>
-                <p className="section-sub-title">High School Education <span className="year">(2003-2013)</span> </p>
+               <Education institute="PES College of Engineering" years='2016-2020' cgpa={8.9} point1="Lorem Ipsum is simply dummy text of the printing and typesetting industry." point2="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
+               <Education institute="Diploma in Mechatronics" years='2013-2016' cgpa={8.2} point1="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout." point2="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."/>
+               
+               <Education institute="High School Education" years='2003-2013' cgpa={8.1} point1="The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested." point2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
             </div>
             <hr />
             <div className="Skill">
                 <p className="section-title">Skills</p>
-                <p className="section-sub-title">Skill 1</p>
-                <p className="section-sub-title">Skill 2</p>
-                <p className="section-sub-title">Skill 3</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>PHP</li>
+                    <li>React.js</li>
+                </ul>
             </div>
         </div>
     )
--- src/RightSection/index.js
+++ src/RightSection/index.js
@@ -1,22 +1,37 @@
 import './index.css'
-
+import Project from './Project/index'
 const RightSection = () =>{
     return(
+       
         <div className="RightSection">
-          <div className="Project">
-                <p className="section-title">Projects</p>
-                <p className="section-sub-title">Project 1</p>
-                <p className="section-sub-title">Project 2</p>
-                <p className="section-sub-title">Project 3</p>
-            </div>
-            <hr/>
-            <div className="Skill">
-                <p className="section-title">Achievements</p>
-                <p className="section-sub-title">Achievement 1</p>
-                <p className="section-sub-title">Achievement 2</p>
-                <p className="section-sub-title">Achievement 3</p>
-            </div>
-         </div>
+        <div className="Project">
+            <p className="section-title">Projects</p>
+        <Project name="Calculator App" years='2018' techStack={['HTML', 'CSS', 'Bootstrap', 'Javascript' ]}  point1="Lorem Ipsum is simply dummy text of the printing and typesetting industry." point2="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
+
+        <Project name="Registration App" years='2019' techStack={['HTML', 'CSS', 'Bootstrap', 'Javascript' ]}   point1="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout." point2="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."/>
+        
+        <Project name="Order status management" years='2020' techStack={['HTML', 'CSS', 'Bootstrap', 'React.js' ]}   point1="The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested." point2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+        </div>
+        <hr />
+        {/* <div className="Achievements">
+            <p className="section-title">Achievements</p>
+            <ul>
+                <li>Secured </li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>PHP</li>
+                <li>React.js</li>
+            </ul>
+        </div> */}
+        <div className="Languages">
+            <p className="section-title">Languages</p>
+            <ul>
+                <li>English </li>
+                <li>Hindi</li>
+                <li>Marathi</li> 
+            </ul>
+        </div>
+        </div>
     )
 }
 
